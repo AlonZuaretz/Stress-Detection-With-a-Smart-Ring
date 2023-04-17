6563359e3744392d98198524b7e977e75ee84b7d
AlonZuaretz <alon.zuaretz@campus.technion.ac.il>
2023-04-17 19:40:55 +0300

    preprocessing and main template
    added very basic preprocessing to data, need to fetch data from python

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,1 +1,15 @@
-disp("hello")
+% Constant Parameters:
+fs = 3; %Hertz
+
+
+
+% Fetch data:
+
+% t =
+% raw = 
+
+
+% Preprocess:
+order = 128;
+cutoff_freq = 0.5;
+
---
+++ Matlab/preprocess.m
@@ -0,0 +1,9 @@
+function [y] = preprocess(x, t, fs, order, cutoff_freq)
+
+% Filter the data:
+normalized_freq = cutoff_freq/(fs/2);
+b = fir1(order, normalized_freq);
+
+filtered_x = filter(b,1,x);
+
+y = filtered_x;
